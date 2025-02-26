1c0fa6cb9a650672584aa5efe867761f6d55d7b8
Kapil552 <kapilkurmi.adt@gmail.com>
2025-02-26 19:23:02 +0530

    second commit

--- Jenkins-Program/Main.java
+++ Jenkins-Program/Main.java
@@ -4,6 +4,7 @@ class Main{
     public static void main(String[] args) {
         System.out.println("This is java program");
         System.out.println("output from java program");
+        System.out.println("output from java program2");
         System.out.println(new Date());
     }
 }
